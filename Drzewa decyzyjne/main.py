790d8a207409fc9d1741a609d9d4a7f3036bc0a6
Aleksandra Kramek <147414924+olakramek@users.noreply.github.com>
2024-05-13 21:51:16 +0200

    Create main.py

---
+++ Drzewa decyzyjne/main.py
@@ -0,0 +1,26 @@
+
+from sklearn.model_selection import train_test_split
+from decision_tree import DecisionTree
+from sklearn import datasets
+import numpy as np
+
+
+
+
+
+
+data = datasets.load_iris()
+X, y = data.data, data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+clf = DecisionTree(max_depth=10)
+clf.fit(X_train, y_train)
+predictions = clf.predict(X_test)
+
+def accuracy(y_true, y_pred):
+    return np.sum(y_true == y_pred) / len(y_true)
+
+
+acc = accuracy(y_test, predictions)
+print(acc)
